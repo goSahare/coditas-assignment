2792b0595a84b1a1738fd921166809861ae8487b
gosahare <gouravsahare@gmail.com>
2019-04-29 09:35:16 +0530

    pipe changes

--- src/app/pipes/sort.pipe.ts
+++ src/app/pipes/sort.pipe.ts
@@ -1,24 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-
 @Pipe({
   name: 'sort'
 })
 export class SortPipe implements PipeTransform {
 
-  transform(obj: any, field?: any): any {
-  	console.log(obj, field);
-  	if (obj ===undefined ) return null;
-  	
+  transform(obj: any, field: string): any {
   	if (field == 'Name (Z-A)')
-  		obj.value.sort((x,y) => y.login.localeCompare(x.login))
+  		obj.sort((x,y) => y.login.localeCompare(x.login));
   	if (field == 'Rank Up')
-  		obj.value.sort((x,y) => x.score - y.score)
+  		obj.sort((x,y) => x.score - y.score)
   	if (field == 'Rank Down')
-  		obj.value.sort((x,y) => y.score - x.score)
-  	else
-  		obj.value.sort((x,y) => x.login.localeCompare(y.login));
-
+  		obj.sort((x,y) => y.score - x.score)
+  	else if(field == 'Name (A-Z)')
+  	 	obj.sort((x,y) => x.login.localeCompare(y.login));
     return obj;
   }
 }
